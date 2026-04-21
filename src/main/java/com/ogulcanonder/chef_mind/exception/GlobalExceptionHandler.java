90be6ecd5dda912b4c2c98f8de42618e4ba8f296
Oğulcan Önder <canonder9246@gmail.com>
2026-04-21 17:32:09 +0300

    Update exception handler for unique errors

--- src/main/java/com/ogulcanonder/chef_mind/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ogulcanonder/chef_mind/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ogulcanonder.chef_mind.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,8 +18,8 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(ResourceNotUniqueException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotUniqueException(ResourceNotUniqueException ex) {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotUniqueException(DataIntegrityViolationException ex) {
         ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
                 ex.getMessage(), "Unique Error");
         return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
