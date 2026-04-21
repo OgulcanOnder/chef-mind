18e67f0ba5edae8e372b6ea688a6ebe791015122
Oğulcan Önder <canonder9246@gmail.com>
2026-04-21 16:44:28 +0300

    Standardize the controller structure, implement update and delete operations, and add layer-based validation.

--- src/main/java/com/ogulcanonder/chef_mind/service/IIngredientCategoryService.java
+++ src/main/java/com/ogulcanonder/chef_mind/service/IIngredientCategoryService.java
@@ -7,7 +7,9 @@ import com.ogulcanonder.chef_mind.model.IngredientCategory;
 import java.util.List;
 
 public interface IIngredientCategoryService {
-    public DtoIngredientCategoryResponse createIngredientCategory(DtoIngredientCategoryRequest dtoIngredientCategoryRequest);
-    public List<DtoIngredientCategoryResponse> getAllIngredientCategory();
-    public IngredientCategory getIngredientCategoryById(Long id);
+    public DtoIngredientCategoryResponse create(DtoIngredientCategoryRequest dtoIngredientCategoryRequest);
+    public List<DtoIngredientCategoryResponse> getAll();
+    public IngredientCategory findById(Long id);
+    public void updateById(Long id, DtoIngredientCategoryRequest dtoIngredientCategoryRequest);
+    public void deleteById(Long id);
 }
