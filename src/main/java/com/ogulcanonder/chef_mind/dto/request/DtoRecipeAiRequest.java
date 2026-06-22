bcf3b7e44733027dc016e0da28f69ca6019978c8
Oğulcan Önder <canonder9246@gmail.com>
2026-06-22 09:29:18 +0300

    recommended validation added

--- src/main/java/com/ogulcanonder/chef_mind/dto/request/DtoRecipeAiRequest.java
+++ src/main/java/com/ogulcanonder/chef_mind/dto/request/DtoRecipeAiRequest.java
@@ -1,5 +1,6 @@
 package com.ogulcanonder.chef_mind.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.List;
 public class DtoRecipeAiRequest {
 
     @NotEmpty(message = "Not ingredients cannot be empty")
-    List<String>ingredients;
+    List<@NotBlank(message = "Ingredient name cannot be blank") String>ingredients;
 
     public List<String> getIngredients() {
         return ingredients;
